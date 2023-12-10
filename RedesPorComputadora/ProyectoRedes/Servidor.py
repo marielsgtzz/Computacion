6d158fc5cbe2762abf75c767b1d99ffe735ba6b0
MarielSoGuZa <marielsgtzz@gmail.com>
2023-12-10 15:20:45 -0600

    Manda al cliente solicitud de chat

--- RedesPorComputadora/ProyectoRedes/Servidor.py
+++ RedesPorComputadora/ProyectoRedes/Servidor.py
@@ -14,6 +14,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #le dice al kernel 
 server.bind(ADDR) #binding del socket con el server
 
 clientes = {} #Lista que tendra a todos los clientes
+conversaciones = {} # Estructura {{idConv, [alias1,alias2]}}
 
 #Se maneja la conexion cliente - servidor de forma individual e independiente a las demas
 def handle_client(conn, addr):
@@ -25,25 +26,57 @@ def handle_client(conn, addr):
     print(f"Usuario {alias} conectado desde {addr}")
 
     connected = True
-    cantMsg = 0
     # Bucle principal para recibir mensajes
     while connected:
-        if cantMsg == 0:
-            conn.send("Con que alias quieres hablar?".encode(FORMAT))
+        conn.send("Con que alias quieres hablar?\n".encode(FORMAT))
+        alias_destino_length = int(conn.recv(HEADER).decode(FORMAT))
+        alias_destino = conn.recv(alias_destino_length).decode(FORMAT)
+
+        if alias_destino in clientes:
+            conn_destino, _ = clientes[alias_destino]
+            print(f"{alias} quiere hablar con {alias_destino}")
+            conn_destino.send(f"{alias} quiere hablar contigo. Aceptas? (s/n)\n".encode(FORMAT))
         else:
-            conn.send(" ".encode(FORMAT)) # Para que pueda mandar varios mensajes sin que pregunte cada vez por el alias
-        # Primero recibe la longitud del mensaje
+            conn.send("[ERROR] Usuario no encontrado.\n".encode(FORMAT))
+
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            # Luego recibe el mensaje de esa longitud
             msg = conn.recv(msg_length).decode(FORMAT)
-            cantMsg += 1
-            # Comprueba si es el mensaje de desconexión
             if msg == DISCONNECT_MESSAGE:
                 connected = False
-            else:
-                print(f"[{alias}]: {msg}")
+
+        # # Primero recibe la longitud del mensaje
+        # msg_length = conn.recv(HEADER).decode(FORMAT)
+        # if msg_length:
+        #         msg_length = int(msg_length)
+        #         # Luego recibe el mensaje de esa longitud
+        #         msg = conn.recv(msg_length).decode(FORMAT)
+        #         # Comprueba si es el mensaje de desconexión
+        #         if msg == DISCONNECT_MESSAGE:
+        #             connected = False
+        #         else:
+        #             #print(f"[{alias}]: {msg}")
+        #             #if cantMsg == 0:
+        #             conn.send("Con que alias quieres hablar?\n".encode(FORMAT))
+        #             alias_destino = conn.recv(HEADER).decode(FORMAT)
+        #             cantMsg += 1
+        #             if alias_destino in clientes:
+        #                 # Obtiene la conexión del destinatario
+        #                 conn_destino, addr_destino = clientes[alias_destino]
+        #                 print(f"{alias} quiere hablar con {alias_destino} el cual esta en {clientes[alias_destino][1]}")
+        #                 #conn.send(str(clientes[alias_destino][1]).encode(FORMAT))
+        #                 conn_destino.send(f"{alias} quiere hablar contigo. Aceptas? (s/n)".encode(FORMAT))
+        #             else: #No esta el alias destinatario conectado
+        #                 conn.send("[ERROR] Usuario no encontrado.".encode(FORMAT))
+
+    
+                
+        # else:
+        #     conn.send(" ".encode(FORMAT)) # Para que pueda mandar varios mensajes sin que pregunte cada vez por el alias
+        #     # Primero recibe la longitud del mensaje
+        #     msg_length = conn.recv(HEADER).decode(FORMAT)
+            
 
 
     conn.close() #Cierra la conexión
